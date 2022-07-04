4051e57a0fb10e59e120d43904bb3dbcff044eac
g0d1-prog <pedrobarros232@hotmail.com>
2022-07-04 15:58:56 -0300

    Merging the frontend with the backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('api/', include('contactForm.urls'))
 ]
 
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
